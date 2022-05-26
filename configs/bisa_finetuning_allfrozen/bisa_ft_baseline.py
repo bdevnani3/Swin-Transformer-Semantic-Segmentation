93af69d27d3d24accb8e9201f49dddb743459a6c
bdevnani3 <bhavika.devnani@gmail.com>
2022-05-25 23:53:59 -0400

    update schedule

--- configs/bisa_finetuning_allfrozen/bisa_ft_baseline.py
+++ configs/bisa_finetuning_allfrozen/bisa_ft_baseline.py
@@ -2,7 +2,7 @@ _base_ = [
     "../_base_/models/upernet_bisa_swin_transformer.py",
     "../_base_/datasets/ade20k.py",
     "../_base_/default_runtime.py",
-    "../_base_/schedules/schedule_80k.py",  #CHANGE TO 240 in second stage
+    "../_base_/schedules/schedule_40k.py",  #CHANGE TO 240 in second stage
 ]
 
 
